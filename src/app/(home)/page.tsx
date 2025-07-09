710cf1c9455e4c8f9c448af711530a98cce7a418
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-09 14:30:31 +0900

    [DP-1] feat: add default style for text

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-secondary-500 text-body-sm text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-secondary-500 caption-lg text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
