bdcccf0001159f56670f087a48f9635d75222684
illustermin <2002hongmj@naver.com>
2025-07-11 21:31:20 +0900

    [DP-44] feat: badge 공통 컴포넌트 구현 및 variant 분리 #16

--- src/components/feature/chat/ChatContent.tsx
+++ src/components/feature/chat/ChatContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ButtonComponent } from "@/components/common/button";
+import { BadgeComponent } from "@components/common/badge";
 
 export default function ChatContent() {
   return (
@@ -25,7 +26,25 @@ export default function ChatContent() {
           </ButtonComponent>
         </div>
       </div>
+      <section>
+        <h2 className="font-bold text-lg">뱃지 테스트</h2>
+        <div className="mt-4 flex flex-wrap gap-2">
+          <BadgeComponent variant="count" size="md">
+            22
+          </BadgeComponent>
 
+          <BadgeComponent variant="label" size="sm">
+            자투리구매
+          </BadgeComponent>
+
+          <BadgeComponent variant="mapcategory" className="bg-color-bg-primary2 text-black">
+            와이파이
+          </BadgeComponent>
+          <BadgeComponent variant="mapcategory" className="bg-color-bg-secondary2 text-white">
+            핫스팟
+          </BadgeComponent>
+        </div>
+      </section>
       <div className="mt-10">
         <p>채팅 내용이 여기에 들어갑니다...</p>
       </div>
