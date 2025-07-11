cc6d21178507fc6d2dadcae92ec562515aca19a8
illustermin <2002hongmj@naver.com>
2025-07-11 16:17:29 +0900

    [DP-44] style: import 구문 정리(한 줄 띄움) #16

--- src/components/common/Badge/BadgeComponent.tsx
+++ src/components/common/Badge/BadgeComponent.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import * as React from "react";
-import { cn } from "@/lib/utils";
 import { badgeVariants } from "./badgeVariants";
 import type { BadgeProps } from "./badge.types";
 import { Badge } from "@ui/badge";
+
+import { cn } from "@/lib/utils";
 
 export function BadgeComponent({ className, variant, ...props }: BadgeProps) {
   return (
