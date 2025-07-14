6943557ce0234ba244c2ab174f06b0947bd8a45c
illustermin <2002hongmj@naver.com>
2025-07-14 13:26:38 +0900

    [DP-55] feat: 팀 tailwind 네이밍 반영 및 버튼 variant/size 구조 개선 #33

--- src/components/common/button/button.types.ts
+++ src/components/common/button/button.types.ts
@@ -3,4 +3,5 @@ import type { ButtonVariantProps } from "./buttonVariants";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariantProps {
   color?: string;
+  isActive?: boolean;
 }
