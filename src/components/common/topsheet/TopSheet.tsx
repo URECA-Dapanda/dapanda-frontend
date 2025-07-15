8c84bbd62ccf7c1b7261b40753ab720d621dca37
illustermin <2002hongmj@naver.com>
2025-07-15 15:35:35 +0900

    [DP-59] feat: topSheet 드래그 영역을 내부로 한정하고 expanded 시 이미지 위치 및 패딩 조정 #41

--- src/components/common/topsheet/TopSheet.tsx
+++ src/components/common/topsheet/TopSheet.tsx
@@ -14,7 +14,7 @@ export default function TopSheet({ type, data, onImageClick }: TopSheetProps) {
   const getImageStyle = (expanded: boolean, type: "post" | "wifi") => {
     if (expanded) {
       return {
-        top: 30,
+        top: 51,
         left: "50%",
         x: "-50%",
         width: 200,
@@ -46,96 +46,101 @@ export default function TopSheet({ type, data, onImageClick }: TopSheetProps) {
 
   return (
     <motion.div
-      className="absolute top-0 w-[375px] bg-secondary shadow-default rounded-b-30 overflow-hidden"
+      className=" absolute top-0 w-[375px] bg-secondary shadow-default rounded-b-30 overflow-hidden"
       animate={{ y: expanded ? -10 : 0 }}
-      transition={{ type: "spring", damping: 20, stiffness: 200 }}
-      drag="y"
-      dragConstraints={{ top: 0, bottom: 100 }}
-      onDragEnd={(event, info) => {
-        if (info.offset.y > 60 || info.velocity.y > 500) {
-          setExpanded(true);
-        } else {
-          setExpanded(false);
-        }
-      }}
+      dragConstraints={{ top: 0, bottom: 0 }}
     >
-      <motion.img
-        src={data.imageUrl}
-        alt="대표 이미지"
-        onClick={onImageClick}
-        className="cursor-zoom-in absolute rounded-12 z-30"
-        animate={getImageStyle(expanded, type)}
-        transition={{ type: "spring", damping: 20, stiffness: 200 }}
-      />
-
       <motion.div
-        className="relative z-10 pl-30 px-4 space-y-1"
-        animate={{
-          paddingTop: expanded ? 240 : type === "wifi" ? 51 : 60,
+        className="w-full h-full"
+        drag="y"
+        transition={{ type: "decay", damping: 20, stiffness: 200 }}
+        dragConstraints={{ top: 0, bottom: 0 }}
+        onDragEnd={(event, info) => {
+          if (info.offset.y > 60 || info.velocity.y > 500) {
+            setExpanded(true);
+          } else {
+            setExpanded(false);
+          }
         }}
-        transition={{ type: "spring", damping: 20, stiffness: 200 }}
       >
-        {type === "post" && data.hasReported && (
-          <>
-            <motion.div
-              className="absolute top-8 right-4 z-20 rounded-full bg-white shadow-default w-30 h-30 flex items-center justify-center"
-              animate={{ y: expanded ? 20 : 0 }}
-              transition={{ type: "spring", damping: 20, stiffness: 200 }}
-            >
-              <Siren className="text-red-500 w-12 h-12" />
-            </motion.div>
-            <div className="space-y-[8px]">
-              <BadgeComponent variant="meta" size="md" className="bg-gray-400">
-                {data.uploadTime} 전
-              </BadgeComponent>
-              <h2 className="h1">{data.title}</h2>
-            </div>
-            <div className="flex flex-col gap-8">
-              <p className="title-md">{data.price}원</p>
-              {expanded && <p className="body-xs">{data.unitPrice}원/100MB</p>}
+        <motion.img
+          src={data.imageUrl}
+          alt="대표 이미지"
+          onClick={onImageClick}
+          className="cursor-zoom-in absolute rounded-12 z-30"
+          animate={getImageStyle(expanded, type)}
+          transition={{ type: "spring", damping: 20, stiffness: 200 }}
+        />
 
-              {data.recentPrice && data.averagePrice && (
-                <div className="flex gap-12 pt-20 flex-wrap">
-                  <BadgeComponent variant="meta" size="md" className="bg-white">
-                    최근거래가: {data.recentPrice}원
-                  </BadgeComponent>
-                  <BadgeComponent variant="meta" size="md" className="bg-white">
-                    평균거래가: {data.averagePrice}원
-                  </BadgeComponent>
-                </div>
-              )}
-            </div>
-          </>
-        )}
-        {type === "wifi" && (
-          <>
-            <div className="space-y-8">
-              <BadgeComponent variant="meta" size="md" className="bg-primary2 text-black">
-                와이파이
-              </BadgeComponent>
-              <h2 className="h2">{data.place}</h2>
-              <p className="caption-md text-gray-500">{data.address}</p>
-            </div>
+        <motion.div
+          className="relative z-10 pl-30 px-4 space-y-1"
+          animate={{
+            paddingTop: expanded ? 260 : type === "wifi" ? 51 : 60,
+          }}
+          transition={{ type: "spring", damping: 20, stiffness: 200 }}
+        >
+          {type === "post" && data.hasReported && (
+            <>
+              <motion.div
+                className="absolute top-8 right-4 z-20 rounded-full bg-white shadow-default w-30 h-30 flex items-center justify-center"
+                animate={{ y: expanded ? 20 : 0 }}
+                transition={{ type: "spring", damping: 20, stiffness: 200 }}
+              >
+                <Siren className="text-red-500 w-12 h-12" />
+              </motion.div>
+              <div className="space-y-[8px]">
+                <BadgeComponent variant="meta" size="md" className="bg-gray-400">
+                  {data.uploadTime} 전
+                </BadgeComponent>
+                <h2 className="h1">{data.title}</h2>
+              </div>
+              <div className="flex flex-col gap-8">
+                <p className="title-md">{data.price}원</p>
+                {expanded && <p className="body-xs">{data.unitPrice}원/100MB</p>}
 
-            <div className="flex flex-col gap-8 pt-12">
-              <p className="body-xs">
-                {data.openTime} ~ {data.closeTime}
-              </p>
-              <p className="body-xs">{data.pricePer10min}원/10분</p>
-              {expanded && <p className="body-sm">{data.description}</p>}
-              {data.recentPrice && data.averagePrice && (
-                <div className="flex gap-12 pt-4 flex-wrap">
-                  <BadgeComponent variant="outlined" size="sm" className="body-xxs">
-                    최근 거래가: {data.recentPrice}원
-                  </BadgeComponent>
-                  <BadgeComponent variant="outlined" size="sm" className="body-xxs">
-                    평균 거래가: {data.averagePrice}원
-                  </BadgeComponent>
-                </div>
-              )}
-            </div>
-          </>
-        )}
+                {data.recentPrice && data.averagePrice && (
+                  <div className="flex gap-12 pt-20 flex-wrap">
+                    <BadgeComponent variant="meta" size="md" className="bg-white">
+                      최근거래가: {data.recentPrice}원
+                    </BadgeComponent>
+                    <BadgeComponent variant="meta" size="md" className="bg-white">
+                      평균거래가: {data.averagePrice}원
+                    </BadgeComponent>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+          {type === "wifi" && (
+            <>
+              <div className="space-y-8">
+                <BadgeComponent variant="meta" size="md" className="bg-primary2 text-black">
+                  와이파이
+                </BadgeComponent>
+                <h2 className="h2">{data.place}</h2>
+                <p className="caption-md text-gray-500">{data.address}</p>
+              </div>
+
+              <div className="flex flex-col gap-8 pt-12">
+                <p className="body-xs">
+                  {data.openTime} ~ {data.closeTime}
+                </p>
+                <p className="body-xs">{data.pricePer10min}원/10분</p>
+                {expanded && <p className="body-sm">{data.description}</p>}
+                {data.recentPrice && data.averagePrice && (
+                  <div className="flex gap-12 pt-4 flex-wrap">
+                    <BadgeComponent variant="outlined" size="sm" className="body-xxs">
+                      최근 거래가: {data.recentPrice}원
+                    </BadgeComponent>
+                    <BadgeComponent variant="outlined" size="sm" className="body-xxs">
+                      평균 거래가: {data.averagePrice}원
+                    </BadgeComponent>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+        </motion.div>
       </motion.div>
 
       {!expanded ? (
@@ -150,7 +155,6 @@ export default function TopSheet({ type, data, onImageClick }: TopSheetProps) {
           <div className="w-[30%] h-1.5 bg-gray-400/60 rounded-full" />
         </div>
       )}
-
       {isModalOpen && (
         <FullScreenModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
           <img
