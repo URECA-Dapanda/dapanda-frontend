55063bc1dce4d350a3eb6318299918be11582824
Jetty-Lee <jetty0188@gmail.com>
2025-07-15 16:28:22 +0900

    [DP-64] fix: bottomsheet open/close animation 수정 #49

--- src/components/common/bottomsheet/BaseBottomSheet.tsx
+++ src/components/common/bottomsheet/BaseBottomSheet.tsx
@@ -43,7 +43,23 @@ export default function BaseBottomSheet({
         />
       )}
 
-      <div className="fixed inset-0 flex items-end justify-center z-30 pointer-events-none">
+      <motion.div className="fixed inset-0 flex items-end justify-center z-30 pointer-events-none"   
+      dragConstraints={{ top: 0, bottom: 0 }}
+      style={
+        {
+          WebkitOverflowScrolling: "touch",
+            touchAction: "pan-y",
+        }
+      }
+      onDragEnd={(event, info) => {
+        if (info.offset.y > 100 || info.velocity.y > 500) {
+          if (variant === "modal") onClose?.();
+          if (variant === "snap") setSheetY(snapHeight);
+        } else {
+          setSheetY(0);
+        }
+      }}
+      drag="y">
         <motion.div
           className="bg-white rounded-t-20 pointer-events-auto p-24"
           style={{
@@ -54,25 +70,13 @@ export default function BaseBottomSheet({
                 : `${MODAL_MAX_HEIGHT}px`,
             marginBottom: variant === "snap" ? `${BOTTOM_OFFSET}px` : "0px",
             overflowY: "auto",
-            WebkitOverflowScrolling: "touch",
-            touchAction: "pan-y",
           }}
           animate={{ y: sheetY }}
           transition={{ type: "spring", damping: 30, stiffness: 300 }}
-          drag="y"
-          dragConstraints={{ top: 0, bottom: 0 }}
-          onDragEnd={(event, info) => {
-            if (info.offset.y > 100 || info.velocity.y > 500) {
-              if (variant === "modal") onClose?.();
-              if (variant === "snap") setSheetY(snapHeight);
-            } else {
-              setSheetY(0);
-            }
-          }}
         >
           {children}
         </motion.div>
-      </div>
+      </motion.div>
     </>
   );
 }
