be1ca2b5cad9527c8874a2553ccc46084f8093cc
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-16 19:30:12 +0900

    [DP-74] feat: finished mypage's main page

--- src/feature/mypage/components/MyPageContent.tsx
+++ src/feature/mypage/components/MyPageContent.tsx
@@ -1,32 +1,29 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import CurrentCashCard from "./sections/CurrentCashCard";
 import TransactionHistory from "./sections/TransactionHistory";
 import { DataUsageDonut } from "./sections/CicularProgressBar";
 import { ButtonComponent } from "@components/common/button";
-import { Rating, RatingButton } from "@components/common/rating/RatingScore";
+import ProfileCard from "./sections/ProfileCard";
 
 export default function MyPageContent() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full p-24 gap-[10px]">
+    <div className="flex flex-col items-center justify-center w-full h-full p-24 gap-12">
+      <ProfileCard />
       <DataUsageDonut current={5.6} total={10} unit="GB" />
       <ButtonComponent variant={"text"} size={"sm"}>
         요금제 추천 보러가기
       </ButtonComponent>
-
-      <Rating defaultValue={3}>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <RatingButton key={index} className="text-amber-300" />
-        ))}
-      </Rating>
       <TransactionHistory />
       <CurrentCashCard isInterection={true} />
       <Link href={"/"}>
         <Image src={"/pandaDic.svg"} alt="PandaDic" width={327} height={170} />
       </Link>
+      <ButtonComponent variant={"outlinePrimary"} className="w-full">
+        로그아웃
+      </ButtonComponent>
     </div>
   );
 }
