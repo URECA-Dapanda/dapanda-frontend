d22b918222243598efd1abb74d5705fc4c0d9356
6v6DS <dasul100@ajou.ac.kr>
2025-07-21 01:10:42 +0900

    [DP-79] fix: 네이버 스크립트 위치 이동 #64

--- src/app/map/layout.tsx
+++ src/app/map/layout.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
+import Script from "next/script";
 import SharedHeader from "@components/common/header/SharedHeader";
 import BottomNavigation from "@components/common/navigation/BottomNavigation";
-import { ReactNode } from "react";
 
 export default function MapLayout({ children }: { children: ReactNode }) {
   return (
@@ -8,7 +9,14 @@ export default function MapLayout({ children }: { children: ReactNode }) {
       <header className="fixed top-0 left-0 right-0 z-50">
         <SharedHeader />
       </header>
-      <main className="pt-[56px] pb-[56px]">{children}</main>
+      <main className="pt-[56px] pb-[56px]">
+        {" "}
+        <Script
+          strategy="beforeInteractive"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID}&submodules=geocoder`}
+        />
+        {children}
+      </main>
       <BottomNavigation />
     </>
   );
