4dd2f8fdadb331173208920382aabeef71092588
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-21 03:17:25 +0900

    [DP-93] fix: remove build error

--- src/app/data/review/layout.tsx
+++ src/app/data/review/layout.tsx
@@ -1,13 +1,5 @@
-import SharedHeader from "@components/common/header/SharedHeader";
-import BottomNavigation from "@components/common/navigation/BottomNavigation";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function ReviewLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative h-[100dvh] w-full">
-      <SharedHeader />
-      <div className=" flex-1 w-full max-w-4xl p-6">{children}</div>
-      <BottomNavigation />
-    </div>
-  );
+  return <Suspense>{children}</Suspense>;
 }
