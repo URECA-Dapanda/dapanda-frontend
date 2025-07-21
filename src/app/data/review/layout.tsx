111cc08f03def405cfce23645e730176d230183e
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-21 14:15:46 +0900

    [DP-93] fix: remove build error

--- src/app/data/review/layout.tsx
+++ src/app/data/review/layout.tsx
@@ -1,5 +1,11 @@
+import SharedHeader from "@components/common/header/SharedHeader";
 import { ReactNode, Suspense } from "react";
 
 export default function ReviewLayout({ children }: { children: ReactNode }) {
-  return <Suspense>{children}</Suspense>;
+  return (
+    <Suspense>
+      <SharedHeader />
+      {children}
+    </Suspense>
+  );
 }
