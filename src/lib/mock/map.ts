5e56b7b0d79c35a7d9b549d58d0592f1ed19119f
illustermin <2002hongmj@naver.com>
2025-07-22 23:10:44 +0900

    [DP-118] feat: map mock delete #93

--- src/lib/mock/map.ts
+++ src/lib/mock/map.ts
@@ -1,30 +1,30 @@
-import { MapType } from "@/feature/map/types/mapType";
+// import { MapType } from "@/feature/map/types/mapType";
 
-export const mockMapList = (center: { lat: number; lng: number }): MapType[] => {
-  return [
-    {
-      id: 1,
-      productId: 1,
-      title: "핫스팟 A",
-      type: "핫스팟",
-      address: "서울시 강남구",
-      updatedAt: "3시간 전",
-      location: `${center.lat + 0.001},${center.lng + 0.001}`,
-      isOpen: true,
-      score: 4.5,
-      price: 300,
-    },
-    {
-      id: 2,
-      productId: 2,
-      title: "와이파이 B",
-      type: "와이파이",
-      address: "서울시 서초구",
-      updatedAt: "3시간 전",
-      location: `${center.lat - 0.001},${center.lng - 0.001}`,
-      isOpen: true,
-      score: 4.2,
-      price: 100,
-    },
-  ];
-};
+// export const mockMapList = (center: { lat: number; lng: number }): MapType[] => {
+//   return [
+//     {
+//       id: 1,
+//       productId: 1,
+//       title: "핫스팟 A",
+//       type: "핫스팟",
+//       address: "서울시 강남구",
+//       updatedAt: "3시간 전",
+//       location: `${center.lat + 0.001},${center.lng + 0.001}`,
+//       isOpen: true,
+//       score: 4.5,
+//       price: 300,
+//     },
+//     {
+//       id: 2,
+//       productId: 2,
+//       title: "와이파이 B",
+//       type: "와이파이",
+//       address: "서울시 서초구",
+//       updatedAt: "3시간 전",
+//       location: `${center.lat - 0.001},${center.lng - 0.001}`,
+//       isOpen: true,
+//       score: 4.2,
+//       price: 100,
+//     },
+//   ];
+// };
