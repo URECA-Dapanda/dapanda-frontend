5ba852748910b9cd0b7f3556024d4adf89197efb
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-23 15:31:45 +0900

    [DP-144]feat: implement-report-dialog (#115)
    * [DP-144] feat: add report modal
    
    * [DP-144] feat: add modal
    
    * [DP-144] feat: add report dialog and trigger
    
    * [DP-144] fix: fix map route
    
    * [DP-144] feat: add complete modal
    
    * [DP-144] feat: add memberName and review report
    
    * [DP-144] fix: fix build error
    
    * [DP-144] feat: revert z index
    
    * [DP-144] feat: add required

---
+++ src/components/common/button/ReportTriggerButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Siren } from "lucide-react";
+import { useCallback, useState } from "react";
+
+import { cn } from "@lib/utils";
+import ReportModal from "../modal/ReportModal";
+
+interface ReportTriggerButtonProps {
+  targetName: string;
+}
+
+export default function ReportTriggerButton({ targetName }: ReportTriggerButtonProps) {
+  const [reportOpen, setReportOpen] = useState(false);
+
+  const handleReportOpen = useCallback(() => setReportOpen(true), []);
+
+  return (
+    <>
+      <button
+        className={cn(
+          "rounded-full w-[25px] h-[25px] flex justify-center items-center",
+          "bg-white text-error"
+        )}
+        onClick={handleReportOpen}
+      >
+        <Siren size={12} className={cn("bg-white text-error")} />
+      </button>
+      <ReportModal isOpen={reportOpen} setIsOpen={setReportOpen} targetName={targetName} />
+    </>
+  );
+}
---
+++ src/components/common/modal/ReportModal.tsx
@@ -0,0 +1,104 @@
+import { Dispatch, SetStateAction, useCallback, useMemo, useRef, useState } from "react";
+import ModalHeader from "./ModalHeader";
+import { Siren } from "lucide-react";
+import InputComponent from "../input/InputComponent";
+import { ButtonComponent } from "../button";
+import BaseModal from "./BaseModal";
+import { usePathname } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { registReport } from "@apis/reportRequest";
+import ReportCompleteModal from "./ReportCompleteModal";
+import { AxiosError } from "axios";
+
+interface ReportModalProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  targetId?: string;
+  targetName: string;
+}
+
+export default function ReportModal({ isOpen, setIsOpen, targetId, targetName }: ReportModalProps) {
+  const path = usePathname().split("/");
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const [isNext, setIsNext] = useState<boolean>(false);
+  const target = useMemo(() => (targetId ? targetId : path.at(-1) ?? ""), [targetId, path]);
+  const targetCategory = useMemo(() => {
+    switch (path.at(1)) {
+      case "data":
+      case "map":
+        return "PRODUCT";
+      case "chat":
+        return "MEMBER";
+      case "mypage":
+        return "REVIEW";
+      default:
+        return "PRODUCT";
+    }
+  }, [path]);
+  const reportMutation = useMutation({
+    mutationFn: () => registReport(target, ref.current?.value ?? "", targetCategory),
+    mutationKey: ["/api/report", target],
+    onSuccess: () => {
+      setIsOpen(false);
+      setIsNext(true);
+    },
+    onError: (e: AxiosError) => {
+      switch (e.status) {
+        case 409:
+          return alert("이미 신고 처리중입니다.");
+        default:
+          console.error(e.status);
+          return alert("신고 처리중 오류가 발생했습니다.");
+      }
+    },
+  });
+
+  const handleClose = useCallback(() => setIsOpen(false), []);
+  const handleReportClick = useCallback(() => {
+    const comment = ref.current?.value;
+    if (!comment || comment === "") {
+      alert("신고 사유를 작성해 주세요.");
+      ref.current?.focus();
+    } else {
+      reportMutation.mutate();
+    }
+  }, [reportMutation]);
+
+  return (
+    <>
+      <BaseModal isOpen={isOpen} onClose={handleClose}>
+        <ModalHeader title="상대방 신고하기" onClose={handleClose} />
+        <div className="flex flex-col justify-center items-center gap-[25px] w-full">
+          <div className="rounded-full w-60 h-60 bg-error flex justify-center items-center">
+            <Siren className="bg-error" size={30} color="white" />
+          </div>
+          <p className="title-sm">
+            {targetName}님을 <span className="text-error">신고</span>하시겠어요?
+          </p>
+          <p className="body-sm text-gray-600 text-center">
+            신고 횟수가 누적되면 <br />
+            상대방의 다판다 이용이 제한돼요.
+          </p>
+          <div className="flex flex-col gap-8 w-full">
+            <p className="w-full text-start body-sm mx-8">신고 사유를 남겨주세요!</p>
+            <InputComponent
+              as="textarea"
+              placeholder="신고 사유를 작성해주세요."
+              ref={ref}
+              required={true}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between gap-8 mt-40">
+          <ButtonComponent className="flex-1" variant={"primary"} onClick={handleReportClick}>
+            신고하기
+          </ButtonComponent>
+          <ButtonComponent className="flex-1" variant={"outlinePrimary"} onClick={handleClose}>
+            취소하기
+          </ButtonComponent>
+        </div>
+      </BaseModal>
+      <ReportCompleteModal isOpen={isNext} setIsOpen={setIsNext} />
+    </>
+  );
+}
