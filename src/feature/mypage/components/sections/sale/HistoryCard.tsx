8ad18716e286980474b202a6a618b6f769e4606a
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-07-24 10:25:15 +0900

    [DP-154] fix: rename card data

--- src/feature/mypage/components/sections/sale/HistoryCard.tsx
+++ src/feature/mypage/components/sections/sale/HistoryCard.tsx
@@ -5,6 +5,7 @@ import { ButtonComponent } from "@components/common/button";
 import ItemCard from "@components/common/card/ItemCard";
 import LayoutBox from "@components/common/container/LayoutBox";
 import { SaleHistoryType } from "@feature/mypage/types/mypageTypes";
+import { formatDataSize } from "@lib/formatters";
 import { formatDateDivider } from "@lib/time";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
@@ -13,6 +14,12 @@ interface HistoryCardProps {
   data: SaleHistoryType;
   size?: "sm" | "lg";
 }
+
+const postTypeGuard: { [key: string]: string } = {
+  MOBILE_DATA: "데이터",
+  WIFI: "와이파이",
+  HOTSPOT: "핫스팟",
+};
 
 export function HistoryCard({ data, size = "sm" }: HistoryCardProps) {
   const router = useRouter();
@@ -26,14 +33,16 @@ export function HistoryCard({ data, size = "sm" }: HistoryCardProps) {
         <LayoutBox layout="flex" direction="row" gap={19}>
           <AvatarIcon size="small" />
           <LayoutBox layout="flex" direction="column" gap={0}>
-            <p className="title-sm">{data.type}</p>
+            <p className="title-sm">{postTypeGuard[data.type]}</p>
             <p className="body-sm">등록 일자: {formatDateDivider(data.createdAt)}</p>
             {data.state !== "ACTIVE" && (
               <p className="body-sm">
                 거래 일자: {data.updatedAt ? formatDateDivider(data.updatedAt) : "--"}
               </p>
             )}
-            <p className="body-sm text-gray-600">거래 상품: {data.type}</p>
+            <p className="body-sm text-gray-600">
+              거래 상품: {`${formatDataSize(data.dataAmount)} ${postTypeGuard[data.type]}`}
+            </p>
           </LayoutBox>
         </LayoutBox>
         {data.state !== "ACTIVE" && (
