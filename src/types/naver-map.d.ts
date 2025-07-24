84dee316a1b4a1b9c6dd9c30c2cc26ea1c1de5c8
6v6DS <142214903+6v6DS@users.noreply.github.com>
2025-07-24 11:15:38 +0900

    [DP-65] fix: 사용하지 않는 type 제거 #50

--- src/types/naver-map.d.ts
+++ src/types/naver-map.d.ts
@@ -4,25 +4,3 @@ export type Lng = number;
 export type Lat = number;
 export type Coordinates = [Lng, Lat];
 
-declare global {
-  interface NaverMapsService {
-    searchAddressByKeyword: (
-      query: string,
-      callback: (
-        status: naver.maps.Service.Status,
-        response: {
-          v2: {
-            addresses: {
-              x: string;
-              y: string;
-              roadAddress?: string;
-              jibunAddress?: string;
-            }[];
-          };
-        }
-      ) => void
-    ) => void;
-  }
-}
-
-export {};
