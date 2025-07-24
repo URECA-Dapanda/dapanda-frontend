dfd3561fa791e5d21a0f78eb805af19318b3ff40
6v6DS <dasul100@ajou.ac.kr>
2025-07-24 09:36:16 +0900

    [DP-65] feat: 와이파이 수정 api #50

--- src/app/map/[detail]/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode, Suspense } from "react";
-
-export default function MapDetailLayout({ children }: { children: ReactNode }) {
-  return <Suspense>{children}</Suspense>;
-}
--- src/app/map/[detail]/page.tsx
+++
@@ -1,5 +0,0 @@
-import MapDetailPage from "@/feature/map/components/pages/MapDetailPage";
-
-export default function Page() {
-  return <MapDetailPage />;
-}
