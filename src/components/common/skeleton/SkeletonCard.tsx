2c241782d42b8b37aa9171095cbda28c5139df2e
illustermin <2002hongmj@naver.com>
2025-07-28 21:55:53 +0900

    [DP-200] feat: skeleton common component 구현 #156

---
+++ src/components/common/skeleton/SkeletonCard.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function SkeletonCard() {
+  return (
+    <div className="flex items-center gap-32 h-full p-8">
+      <Skeleton className="h-10 w-10 rounded-full" />
+
+      <div className="flex-1 space-y-2">
+        <Skeleton className="h-8 w-120" />
+        <div className="space-y-2">
+          <Skeleton className="h-3 w-152" />
+          <Skeleton className="h-3 w-144" />
+        </div>
+      </div>
+
+      <div className="flex flex-col items-end text-right self-start space-y-8 ml-auto">
+        <Skeleton className="h-12 w-20" />
+        <Skeleton className="h-12 w-36" />
+      </div>
+    </div>
+  );
+}
