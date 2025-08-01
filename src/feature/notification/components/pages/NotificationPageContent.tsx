d9a8c5d696f8229d8c3b03b73b06c87f777dd2b8
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-08-02 02:33:07 +0900

    [DP-232] fix: fix notification page

--- src/feature/notification/components/pages/NotificationPageContent.tsx
+++ src/feature/notification/components/pages/NotificationPageContent.tsx
@@ -1,31 +1,38 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import NotificationList, { NotificationItem } from "@/feature/notification/components/sections/NotificationList";
-import { fetchNotifications, deleteNotification } from "@feature/notification/api/requestNotification";
+import NotificationList, {
+  NotificationItem,
+} from "@/feature/notification/components/sections/NotificationList";
+import {
+  fetchNotifications,
+  deleteNotification,
+} from "@feature/notification/api/requestNotification";
 
 export default function NotificationPageContent() {
-    const [items, setItems] = useState<NotificationItem[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState<NotificationItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        fetchNotifications()
-            .then((res) => setItems(res))
-            .catch((err) => console.error("알림 조회 실패", err))
-            .finally(() => setIsLoading(false));
-    }, []);
+  useEffect(() => {
+    fetchNotifications()
+      .then((res) => setItems(res))
+      .catch((err) => console.error("알림 조회 실패", err))
+      .finally(() => setIsLoading(false));
+  }, []);
 
-    const handleDelete = (id: number) => {
-        deleteNotification(id).then(() => {
-          setItems((prev) => prev.filter((item) => item.id !== id));
-        });
-      };
-      
+  const handleDelete = (id: number) => {
+    deleteNotification(id).then(() => {
+      setItems((prev) => prev.filter((item) => item.id !== id));
+    });
+  };
 
-    return (
-        <div className="p-24">
-            <h1 className="body-xl mb-20">알림</h1>
-            {isLoading ? <p>불러오는 중...</p> : <NotificationList items={items} onDelete={handleDelete} />}
-        </div>
-    );
+  return (
+    <div className="pt-safe-top pb-safe-bottom h-main-safe overflow-y-scroll">
+      {isLoading ? (
+        <p>불러오는 중...</p>
+      ) : (
+        <NotificationList items={items} onDelete={handleDelete} />
+      )}
+    </div>
+  );
 }
