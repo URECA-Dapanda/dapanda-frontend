6a056df10c70d8c5a019db752d81cae40cae0ff2
6v6DS <dasul100@ajou.ac.kr>
2025-08-01 19:43:50 +0900

    [DP-260] fix: 탑시트 수정 #226

--- src/app/(feature)/layout.tsx
+++ src/app/(feature)/layout.tsx
@@ -6,7 +6,7 @@ export default function FeatureLayout({ children }: { children: ReactNode }) {
   return (
     <div className="w-[100dvw] lg:w-[600px] mx-auto bg-white flex flex-col min-h-screen">
       <AppHeader />
-      <main className="flex-1 overflow-y-auto">
+      <main className="h-main-safe overflow-y-visible antialiased w-full lg:w-[600px]">
         <Suspense>{children}</Suspense>
       </main>
       <BottomNavigation />
