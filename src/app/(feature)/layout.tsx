b345890c0be9bf314b84c4a4fe0cd8374b3dfe1b
Jetty-Lee <jetty0188@gmail.com>
2025-08-02 00:20:58 +0900

    [DP-263] fix: 등록하기 모달 네비바 뒤로 올라오는것 수정 #233

--- src/app/(feature)/layout.tsx
+++ src/app/(feature)/layout.tsx
@@ -6,7 +6,7 @@ export default function FeatureLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative w-[100dvw] lg:w-[600px] overflow-hidden mx-auto bg-white flex flex-col">
       <AppHeader id="appHead" />
-      <main className="h-main-safe overflow-y-auto antialiased w-full lg:w-[600px]">
+      <main className="h-main-safe overflow-y-visible antialiased w-full lg:w-[600px]">
         <Suspense>{children}</Suspense>
       </main>
       <BottomNavigation />
