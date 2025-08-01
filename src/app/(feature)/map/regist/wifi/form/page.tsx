0daf261f150de1eb2c03cc86c681e90e8a99c2b9
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-08-02 02:21:08 +0900

    [DP-232] fix: fix registerpage

--- src/app/(feature)/map/regist/wifi/form/page.tsx
+++ src/app/(feature)/map/regist/wifi/form/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@feature/map/components/sections/regist/RegisterForm";
 
 export default function WifiRegisterPage() {
   return (
-    <div className="px-24 py-24">
+    <div className="px-24 pt-safe-top pb-safe-bottom overflow-y-scroll h-main-safe">
       <RegisterForm type="wifi" />
     </div>
   );
