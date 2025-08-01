0ef22369e269f66c708c3a6c120276b116505c2c
Jetty-Lee <jetty0188@gmail.com>
2025-08-01 16:15:30 +0900

    [DP-224] refactor: 필요없는 로그 삭제 #176

--- src/feature/notification/components/pages/NotificationPageContent.tsx
+++ src/feature/notification/components/pages/NotificationPageContent.tsx
@@ -8,43 +8,6 @@ export default function NotificationPageContent() {
     const [items, setItems] = useState<NotificationItem[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    //   useEffect(() => {
-    //     // TODO: 서버 연동 예정
-    //     // fetchNotifications().then((res) => {
-    //     //   setItems(res);
-    //     //   setIsLoading(false);
-    //     // });
-    //     const mock: NotificationItem[] = [
-    //       {
-    //         id: "1",
-    //         title: "핫스팟 예약 완료",
-    //         body: "7월 30일 13:00 핫스팟 예약이 완료되었습니다.",
-    //         createdAt: "2025-07-30 11:00",
-    //       },
-    //       {
-    //         id: "2",
-    //         title: "이용 종료 알림",
-    //         body: "7월 30일 13:00 이용이 종료되었습니다.",
-    //         createdAt: "2025-07-30 13:01",
-    //       },
-    //       {
-    //         id: "3",
-    //         title: "와이파이 구매완료 알림",
-    //         body: "7월 30일 13:00 핫스팟 예약이 완료되었습니다.",
-    //         createdAt: "2025-07-30 18:30",
-    //       },
-    //       {
-    //         id: "4",
-    //         title: "데이터 판매완료 알림",
-    //         body: "데이터 1.5GB가 판매되었습니다",
-    //         createdAt: "2025-07-30 20:20",
-    //       },
-    //     ];
-    //     setTimeout(() => {
-    //       setItems(mock);
-    //       setIsLoading(false);
-    //     }, 300);
-    //   }, []);
     useEffect(() => {
         fetchNotifications()
             .then((res) => setItems(res))
