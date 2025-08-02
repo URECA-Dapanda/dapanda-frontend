c9eaff1961f94ebc28b4fe14bbfcc60d28530fd9
6v6DS <142214903+6v6DS@users.noreply.github.com>
2025-08-02 09:49:07 +0900

    [DP-260] fix: 수정 제거 #226

--- src/app/(feature)/layout.tsx
+++ src/app/(feature)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode, Suspense } from "react";
 
 export default function FeatureLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="relative w-[100dvw] lg:w-[600px] overflow-hidden mx-auto bg-white flex flex-col">
+    <div className="relative w-[100dvw] lg:w-[600px] mx-auto bg-white flex flex-col">
       <AppHeader id="appHead" />
       <main className="h-main-safe overflow-y-auto antialiased w-full lg:w-[600px]">
         <Suspense>{children}</Suspense>
