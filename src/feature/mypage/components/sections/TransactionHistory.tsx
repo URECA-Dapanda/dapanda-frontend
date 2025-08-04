4dc69cdc27cf1e5ce032300b0f6824ef4593c3cd
yunhyuk-choi <cyh9355@yonsei.ac.kr>
2025-08-04 10:45:13 +0900

    [DP-280] fix: mypage 구조 변경

--- src/feature/mypage/components/sections/TransactionHistory.tsx
+++ src/feature/mypage/components/sections/TransactionHistory.tsx
@@ -4,13 +4,17 @@ import HistoryButton from "./HistoryButton";
 export default function TransactionHistory() {
   return (
     <div className="flex flex-col items-center justify-between w-full gap-12">
+      <HistoryButton target="cash-history">
+        <Package2 className="w-16 h-16 mr-8" />
+        거래내역
+      </HistoryButton>
       <HistoryButton target="purchase-history">
         <ShoppingCart className="w-16 h-16 mr-8" />
-        구매내역
+        구매목록
       </HistoryButton>
       <HistoryButton target="sale-history">
         <Package2 className="w-16 h-16 mr-8" />
-        판매내역
+        판매목록
       </HistoryButton>
     </div>
   );
