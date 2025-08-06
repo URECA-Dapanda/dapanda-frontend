f5ec2411084a38e9e8034a6689388bac40ba6f22
6v6DS <dasul100@ajou.ac.kr>
2025-08-06 10:00:05 +0900

    [DP-320] feat: 지도 마커 클러스터링 #319

--- src/types/naver-map.d.ts
+++ src/types/naver-map.d.ts
@@ -4,3 +4,31 @@ export type Lng = number;
 export type Lat = number;
 export type Coordinates = [Lng, Lat];
 
+declare global {
+  namespace naver.maps {
+    interface MarkerClusteringOptions {
+      map: naver.maps.Map;
+      markers?: naver.maps.Marker[];
+      gridSize?: number;
+      minClusterSize?: number;
+      maxZoom?: number;
+      disableClickZoom?: boolean;
+      icons?: {
+        content: string;
+        size: naver.maps.Size;
+        anchor: naver.maps.Point;
+      }[];
+    }
+
+    class MarkerClustering {
+      constructor(options: MarkerClusteringOptions);
+
+      getMarkers(): naver.maps.Marker[];
+      setMarkers(markers: naver.maps.Marker[]): void;
+      clear(): void;
+      setMap(map: naver.maps.Map | null): void;
+    }
+  }
+}
+
+export {};
